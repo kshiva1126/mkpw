c59b1c95f79fba989a46db3c9c3be6d26893de35
kshiva1126 <42695879+kshiva1126@users.noreply.github.com>
2019-05-28 00:01:02 +0900

    second commit

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import (
 )
 
 const (
-	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-	letterIdxMask = 0x3F // 63 0b111111
+	letters       = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	letterIdxMask = 0x3F
 )
 
 var (
@@ -27,10 +27,10 @@ func main() {
 		log.Println(err)
 		os.Exit(1)
 	}
-	for i := 0; i < *number; {
+	for i := range buffer {
 		idx := int(buffer[i] & letterIdxMask)
-		if idx < len(letterBytes) {
-			buffer[i] = letterBytes[idx]
+		if idx < len(letters) {
+			buffer[i] = letters[idx]
 			i++
 		} else {
 			if _, err := rand.Read(buffer[i : i+1]); err != nil {
